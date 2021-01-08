5cce4e8dfa21c3b80cb3c552b2b32b47fa4c0cdf
arcticlimer <vinigm.nho@gmail.com>
2021-01-08 18:44:11 -0300

    Implement TCP server with :gen_tcp

--- apps/kv_server/lib/kv_server/application.ex
+++ apps/kv_server/lib/kv_server/application.ex
@@ -1,6 +1,4 @@
 defmodule KVServer.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
@@ -8,12 +6,13 @@ defmodule KVServer.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: KVServer.Worker.start_link(arg)
-      # {KVServer.Worker, arg}
+      {Task.Supervisor, name: KVServer.TaskSupervisor},
+      Supervisor.child_spec(
+        {Task, fn -> KVServer.accept(8080) end},
+        restart: :permanent
+      )
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: KVServer.Supervisor]
     Supervisor.start_link(children, opts)
   end
